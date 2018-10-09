749091066f9b63c7e4938a5aec5824e8cfb49f28
Pablo <pablo.reypedrosa@gmail.com>
2018-10-09 23:41:43 +0200

    Creo estructura para los tests

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Routes } from './routes/routeProvider';
+
+// https://itnext.io/building-restful-web-apis-with-node-js-express-mongodb-and-typescript-part-2-98c34e3513a2
+// https://scotch.io/tutorials/test-a-node-restful-api-with-mocha-and-chai
+// https://tutorialedge.net/typescript/testing-typescript-api-with-mocha-chai/
 
 class App {
 
     public app: express.Application;
+    public routePrv: Routes = new Routes();
 
     constructor() {
         this.app = express();
-        this.config();        
+        this.config();
+        this.routePrv.routes(this.app);
     }
 
     private config(): void{
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
-import app from "./app";
+import app from './app';
+import * as mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/league-static');
+
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+app.listen(PORT, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    
+    return console.log('Express server listening on port ' + PORT);
 })
