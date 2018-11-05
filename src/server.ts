a36c4df1a43eb1ebdf6d918adbe1b7a74c4de81e
Pablo <pablo.reypedrosa@gmail.com>
2018-11-05 01:04:21 +0100

    Credenciales Mongo

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from './app';
 import * as mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/league-static');
+//mongoose.connect('mongodb://localhost:27017/league-static');
+mongoose.connect('mongodb://heroku_62llrqpk:b2v4g31m0h86lq64s18sbnlhc0@ds251223.mlab.com:51223/heroku_62llrqpk');
 
 const PORT = 3000;
 
@@ -9,6 +10,6 @@ app.listen(PORT, (err) => {
     if (err) {
         return console.log(err);
     }
-    
+
     return console.log('Express server listening on port ' + PORT);
 })
