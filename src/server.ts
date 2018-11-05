fe0a1bd4d180cc4a4464e6e53416ab1d38572a2b
Pablo <pablo.reypedrosa@gmail.com>
2018-11-05 01:43:51 +0100

    Elimino conexión con MongoDB

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import app from './app';
 import * as mongoose from 'mongoose';
 
 //mongoose.connect('mongodb://localhost:27017/league-static');
-mongoose.connect('mongodb://heroku_62llrqpk:b2v4g31m0h86lq64s18sbnlhc0@ds251223.mlab.com:51223/heroku_62llrqpk');
 
 const PORT = process.env.PORT || 3000;
 
