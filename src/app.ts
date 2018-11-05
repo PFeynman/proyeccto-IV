260241702dfc2995ec1bd71ce64dcfd9ef1ed40d
Pablo <pablo.reypedrosa@gmail.com>
2018-11-05 01:14:10 +0100

    Elimino comentarios

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,6 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Routes } from './routes/routeProvider';
 
-// https://itnext.io/building-restful-web-apis-with-node-js-express-mongodb-and-typescript-part-2-98c34e3513a2
-// https://scotch.io/tutorials/test-a-node-restful-api-with-mocha-and-chai
-// https://tutorialedge.net/typescript/testing-typescript-api-with-mocha-chai/
-
 class App {
 
     public app: express.Application;
