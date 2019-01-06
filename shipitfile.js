821a3d9d3daeba2418f2812759a2d3797f710ccd
Pablo <pablo.reypedrosa@gmail.com>
2019-01-06 05:46:32 +0100

    Añado comando para introducir datos base en la base de datos

--- shipitfile.js
+++ shipitfile.js
@@ -5,11 +5,11 @@ module.exports = shipit => {
     default: {
       deployTo: '/var/apps/leaguestatic',
       repositoryUrl: 'https://github.com/PFeynman/proyecto-iv',
-      key: '~/.ssh/id_rsa',
+      key: '~/.ssh/deploy',
       keepReleases: '1',
     },
     staging: {
-      servers: 'vagrant@188.166.12.203',
+      servers: 'deploy@142.93.235.169',
     },
   })
 
@@ -19,5 +19,9 @@ module.exports = shipit => {
 
   shipit.task('start', async () => {
     return shipit.remote('npm start', {cwd: '/var/apps/leaguestatic/current'})
+  })
+
+  shipit.task('initDB', async () => {
+    return shipit.remote('mongoimport data/champions.json --db league-static -c champions && mongoimport data/items.json --db league-static -c items', {cwd: '/var/apps/leaguestatic/current'})
   })
 }
