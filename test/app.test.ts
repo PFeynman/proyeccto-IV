48e22ce3c3d3a37efda03769b9936a56b314aff1
Pablo <pablo.reypedrosa@gmail.com>
2019-01-06 23:48:27 +0100

    Arreglo tests

--- test/app.test.ts
+++ test/app.test.ts
@@ -9,31 +9,31 @@ import app from '../src/app';
 
 describe('Check champion API routes', () => {
   it('should return response to GET all champions request', () => {
-    return chai.request(app).get('/champions')
+    chai.request(app).get('/champions')
       .then(res => {
         chai.expect(res).to.have.status(200);
       })
   })
   it('should return response to POST champions request', () => {
-    return chai.request(app).post('/champions')
+    chai.request(app).post('/champions')
       .then(res => {
         chai.expect(res).to.have.status(200);
       })
   })
   it('should return response to GET specific champion request', () => {
-    return chai.request(app).get('/champion/100')
+    chai.request(app).get('/champion/Aatrox')
       .then(res => {
         chai.expect(res.status).to.equal(200);
       })
   })
   it('should return response to PUT champion request', () => {
-    return chai.request(app).put('/champion/100')
+    chai.request(app).put('/champion/100')
       .then(res => {
         chai.expect(res.status).to.equal(200);
       })
   })
   it('should return response to DELETE champion request', () => {
-    return chai.request(app).del('/champion/100')
+    chai.request(app).del('/champion/100')
       .then(res => {
         chai.expect(res.status).to.equal(200);
       })
@@ -43,31 +43,31 @@ describe('Check champion API routes', () => {
 
 describe('Check item API routes', () => {
   it('should return response to GET all items request', () => {
-    return chai.request(app).get('/items')
+    chai.request(app).get('/items')
       .then(res => {
         chai.expect(res).to.have.status(200);
       })
   })
   it('should return response to POST items request', () => {
-    return chai.request(app).post('/items')
+    chai.request(app).post('/items')
       .then(res => {
         chai.expect(res).to.have.status(200);
       })
   })
   it('should return response to GET specific item request', () => {
-    return chai.request(app).get('/item/100')
+    chai.request(app).get('/item/1001')
       .then(res => {
         chai.expect(res.status).to.equal(200);
       })
   })
   it('should return response to PUT item request', () => {
-    return chai.request(app).put('/item/100')
+    chai.request(app).put('/item/100')
       .then(res => {
         chai.expect(res.status).to.equal(200);
       })
   })
   it('should return response to DELETE item request', () => {
-    return chai.request(app).del('/item/100')
+    chai.request(app).del('/item/1001')
       .then(res => {
         chai.expect(res.status).to.equal(200);
       })
